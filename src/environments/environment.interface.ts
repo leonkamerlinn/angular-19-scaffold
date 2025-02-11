3b683330f27706be1eb3d318b16b10d70c60e2ad
leon <leonkamerlinn@gmail.com>
2025-02-11 15:30:17 +0100

    added enviromets and paths to tsconfig

---
+++ src/environments/environment.interface.ts
@@ -0,0 +1,15 @@
+export interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  databaseURL: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+export interface Environment {
+  production: boolean;
+  apiUrl: string;
+  firebaseConfig: FirebaseConfig;
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+import { Environment } from '@environments/environment.interface';
+
+export const environment: Environment = {
+  production: true,
+  apiUrl: '',
+  firebaseConfig: {
+    apiKey: 'AIzaSyAZGpJbmtzJm7Kps1QkDCE-c-wMrzxz-3s',
+    authDomain: 'crypto-98352.firebaseapp.com',
+    databaseURL: 'https://crypto-98352-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'crypto-98352',
+    storageBucket: 'crypto-98352.appspot.com',
+    messagingSenderId: '1069215389165',
+    appId: '1:1069215389165:web:e7d868884754793d9b43e4',
+    measurementId: 'G-4LQCR9KM2R',
+  },
+};
