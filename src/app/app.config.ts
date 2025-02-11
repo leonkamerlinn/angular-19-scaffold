35f77e333ee3041a7be429894c63270e8007d33e
leon <leonkamerlinn@gmail.com>
2025-02-11 15:48:27 +0100

    app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,43 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideRouterStore } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { RouterSerializer } from '@app-core/router-serializer';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '@environments/environment';
+import { initializeAppFactory } from '@app-core/app-initializer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync(), provideStore(), provideEffects(), provideRouterStore()]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideStore(),
+    provideEffects(),
+    provideRouterStore({ serializer: RouterSerializer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeAppFactory,
+      multi: true,
+      deps: [HttpClient],
+    },
+
+    importProvidersFrom([AngularFireModule.initializeApp(environment.firebaseConfig)]),
+  ]
 };
