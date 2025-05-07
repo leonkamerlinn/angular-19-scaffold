ba280538a0b4d27e8afbe1a291127f73c4bdad6a
leon <leonkamerlinn@gmail.com>
2025-05-07 22:36:10 +0200

    added icons to variable

--- src/app/features/newsletter/newsletter.component.ts
+++ src/app/features/newsletter/newsletter.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButton} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CALENDAR_DAYS_ICON_PATH } from '@app-core/icons/calendar-days-icon';
+import { HAND_THUMB_UP_ICON_PATH } from '@app-core/icons/hand-thumb-up-icon';
 
 interface NewsletterFeature {
   icon: string;
@@ -11,18 +16,24 @@ interface NewsletterFeature {
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [CommonModule, MatButton],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './newsletter.component.html'
 })
 export class NewsletterComponent {
   features: NewsletterFeature[] = [
     {
-      icon: 'M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z',
+      icon: CALENDAR_DAYS_ICON_PATH,
       title: 'Weekly articles',
       description: 'Non laboris consequat cupidatat laborum magna. Eiusmod non irure cupidatat duis commodo amet.'
     },
     {
-      icon: 'M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002',
+      icon: HAND_THUMB_UP_ICON_PATH,
       title: 'No spam',
       description: 'Officia excepteur ullamco ut sint duis proident non adipisicing. Voluptate incididunt anim.'
     }
