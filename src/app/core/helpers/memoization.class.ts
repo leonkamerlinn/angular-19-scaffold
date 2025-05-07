c7c739ac4bef1f606fbf025f61063152e57608d7
leon <leonkamerlinn@gmail.com>
2025-05-07 17:36:56 +0200

    Refactoring memoization.class.ts

--- src/app/core/helpers/memoization.class.ts
+++ src/app/core/helpers/memoization.class.ts
@@ -1,26 +1,41 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export class Memoization {
   private static INSTANCE: Memoization | null = null;
-  private readonly _hashMap: Map<string, any>;
+  private readonly _hashMap: Map<string, unknown>;
 
-  constructor() {
-    this._hashMap = new Map<string, any>();
+  private constructor() {
+    this._hashMap = new Map<string, unknown>();
   }
 
   setValue<T>(key: string, value: T, override = true): boolean {
     if (override || !this._hashMap.has(key)) {
-      this._hashMap.set(key, value as T);
+      this._hashMap.set(key, value);
 
       return true;
     }
 
     return false;
   }
-  getValue<T>(key: string, value?: T): T {
-    if (!this._hashMap.has(key)) {
-      this._hashMap.set(key, value as T);
-    }
 
+  /**
+   * Retrieves a value from the cache.
+   * @param key The key of the value to retrieve.
+   * @returns The cached value or undefined if the key is not found.
+   */
+  getValue<T>(key: string): T | undefined {
+    return this._hashMap.get(key) as T | undefined;
+  }
+
+  /**
+   * Retrieves a value from the cache. If the key is not found,
+   * it uses the defaultValueProvider to compute, store, and return a default value.
+   * @param key The key of the value to retrieve.
+   * @param defaultValueProvider A function that provides the default value if the key is not found.
+   * @returns The cached value or the newly computed and cached default value.
+   */
+  getOrSet<T>(key: string, defaultValueProvider: () => T): T {
+    if (!this._hashMap.has(key)) {
+      this._hashMap.set(key, defaultValueProvider());
+    }
     return this._hashMap.get(key) as T;
   }
 
